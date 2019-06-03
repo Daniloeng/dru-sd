d6999ff59502575a9ca965b0696916c153eb66d0
GILSON da Silva <gilsonsilva.cintra@gmail.com>
2019-06-03 19:20:58 -0300

    atualizacao DRURepository findbyNome

--- src/main/java/io/spring/repository/DRURepository.java
+++ src/main/java/io/spring/repository/DRURepository.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import io.spring.entity.DRU;
+import io.spring.entity.Perfil;
 
 public interface DRURepository extends MongoRepository<DRU, String> {
 
     List<DRU> findByNomeLikeIgnoreCase(String nome);
        
     DRU findByEmail(String email);
+    
+    DRU findByNome(String nome);
 
 }
